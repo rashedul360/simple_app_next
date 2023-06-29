e838e5016a93b2d57ae2f78b4088527d9934c8b2
Rashedul islam <rashedul360rs@gmail.com>
2023-06-29 20:39:10 +0600

    hello added

--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,7 @@ export default function Home() {
       <h2 className="text-base font-semibold leading-7 text-white-900">
        Profile
       </h2>
-      <p className="mt-1 text-sm leading-6 text-gray-600">
-       This information will be displayed publicly so be careful what you share.
-      </p>
+      <p className="mt-1 text-sm leading-6 text-gray-600">helo</p>
 
       <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
        <div className="sm:col-span-4">
